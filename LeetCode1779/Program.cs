b006ff3088d5b0be6eec7f1ad2e7694195e0f283
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-04-25 17:24:26 +0200

    Add 1779 problem solution

---
+++ LeetCode1779/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode1779
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3; int y = 4;
+            int[][] points = new int[][] {
+                new int[] { 1, 2 },
+            };
+
+            Console.WriteLine(NearestValidPoint(x, y, points));
+
+
+            Console.ReadLine();
+        }
+
+        public static int NearestValidPoint(int x, int y, int[][] points)
+        {
+            List<int> pointsLength = new List<int>();
+            List<int> pointsIndexes = new List<int>();
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (points[i][0] == x || points[i][1] == y)
+                {
+                    int tmp = Math.Abs(points[i][0] - x) + Math.Abs(points[i][1] - y);
+                    pointsLength.Add(tmp);
+                    pointsIndexes.Add(i);
+                }
+            }
+
+            if(pointsLength.Count == 0) 
+            {
+                return -1;
+            }
+
+            int tmpNumber = pointsLength.Min();
+            int index = pointsLength.FindIndex(a => a == tmpNumber);
+
+            return pointsIndexes[index];
+        }
+    }
+}
---
+++ LeetCode189/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode189
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] datas = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            Rotate(datas, 3);
+
+            Console.ReadLine();
+        }
+
+        public static void Rotate(int[] nums, int k)
+        {
+            List<int> list = nums.ToList<int>();
+
+            for (int i = 0; i < k; i++)
+            {
+                int tmp = list[list.Count - 1];
+                list.Insert(0, tmp);
+                list.RemoveAt(list.Count - 1);
+            }
+
+            nums = list.ToArray();
+        }
+    }
+}
