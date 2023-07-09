b3807ef30f911d8db065a05c592535a12c68e306
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-07-09 13:13:35 +0200

    Add 48 problem solution

--- LeetCode48/Program.cs
+++ LeetCode48/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,16 +16,17 @@ namespace LeetCode48
 
         public static void Rotate(int[][] matrix)
         {
-            int n = matrix.Length;
+            int length = matrix.Length;
 
-            for (int i = 0; i < (n/2); i++) 
-            { 
-                for (int j = n; j > 1; j=-2)
+            for (int i = 0; i < (length / 2); i++) 
+            {
+                for (int j = i; j < length - i - 1; j++)
                 {
-                    for (int k = 0; k < j; k++)
-                    {
-
-                    }
+                    int tmp = matrix[i][j];
+                    matrix[i][j] = matrix[length - j - 1][i];
+                    matrix[length - j - 1][i] = matrix[length - i - 1][length - j - 1];
+                    matrix[length - i - 1][length - j - 1] = matrix[j][length - 1 - i];
+                    matrix[j][length - 1 - i] = tmp;
                 }
             }
         }
